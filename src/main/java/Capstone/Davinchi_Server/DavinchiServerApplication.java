b40eade43362ee8d178233c421c807b8caa075e2
zzansol <chsk9912@gmail.com>
2024-04-14 16:58:56 +0900

    :tada: feat: 프로젝트 생성

---
+++ src/main/java/Capstone/Davinchi_Server/DavinchiServerApplication.java
@@ -0,0 +1,13 @@
+package Capstone.Davinchi_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavinchiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavinchiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/Davinchi_Server/DavinchiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.Davinchi_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinchiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
