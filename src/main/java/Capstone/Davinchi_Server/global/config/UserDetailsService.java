5a37279a3fd48b7bcc078c932c5b116c2257f948
YWSIK <yuusik818@gmail.com>
2024-05-05 16:52:28 +0900

    :sparkles:

---
+++ src/main/java/Capstone/Davinchi_Server/global/config/UserDetailsService.java
@@ -0,0 +1,23 @@
+package Capstone.Davinchi_Server.global.config;
+
+import Capstone.Davinchi_Server.global.exception.ApiException;
+import Capstone.Davinchi_Server.global.exception.ApiResponseStatus;
+import Capstone.Davinchi_Server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws ApiException {
+        return userRepository.findByEmail(username).orElseThrow(
+                () -> new ApiException(ApiResponseStatus.BAD_REQUEST));
+    }
+}
---
+++ src/main/java/Capstone/Davinchi_Server/user/entity/RefreshToken.java
@@ -0,0 +1,34 @@
+package Capstone.Davinchi_Server.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "refresh_token")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_token_id", unique = true, nullable = false)
+    private Long refreshTokenId;
+
+    @Column(name = "refresh_token", unique = true)
+    private String refreshToken;
+
+    @Column(name = "key_id")
+    private String keyId;
+
+
+    @Builder
+    public RefreshToken(String keyId, String refreshToken) {
+        this.keyId = keyId;
+        this.refreshToken = refreshToken;
+    }
+
+    public void update(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
