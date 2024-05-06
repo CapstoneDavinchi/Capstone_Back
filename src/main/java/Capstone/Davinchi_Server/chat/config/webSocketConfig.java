47103b0d1da93e628874d35e4bf3ab2425f83e0d
이다빈 <dlekqls6204@naver.com>
2024-05-06 19:55:57 +0900

    :sparkles: #16 [feat] 웹소켓 config 구현

---
+++ src/main/java/Capstone/Davinchi_Server/chat/config/webSocketConfig.java
@@ -0,0 +1,20 @@
+package Capstone.Davinchi_Server.chat.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class webSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
