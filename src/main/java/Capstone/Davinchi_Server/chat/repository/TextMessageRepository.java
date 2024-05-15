e14ab374244ac99a5e8ff97f34b7ee9164314aff
이다빈 <dlekqls6204@naver.com>
2024-05-16 04:56:56 +0900

    :zap: fix: repository 수정 어노테이션 추가 Resolves: #16

--- src/main/java/Capstone/Davinchi_Server/chat/repository/TextMessageRepository.java
+++ src/main/java/Capstone/Davinchi_Server/chat/repository/TextMessageRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TextMessageRepository extends JpaRepository<TextMessage, Long> {
     @Query("select t from TextMessage t where t.chatRoom.chatRoomId = :roomId")
     List<TextMessage> findMessagesByRoomId(@Param("roomId") String roomId);
