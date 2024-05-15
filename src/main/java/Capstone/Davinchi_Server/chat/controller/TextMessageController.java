4f9a664918b05c3dee278f8f43e71a6ead5564c1
이다빈 <dlekqls6204@naver.com>
2024-05-16 05:01:01 +0900

    :zap: fix: 채팅방 입장, 채팅창 입력 로직 개선 sub/pub uri, Payload 수정 Resolves: #16

--- src/main/java/Capstone/Davinchi_Server/chat/controller/TextMessageController.java
+++ src/main/java/Capstone/Davinchi_Server/chat/controller/TextMessageController.java
@@ -1,17 +1,16 @@
 package Capstone.Davinchi_Server.chat.controller;
 
+import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageReq;
+import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageRes;
 import Capstone.Davinchi_Server.chat.entity.TextMessage;
-import Capstone.Davinchi_Server.chat.service.ChatRoomService;
-import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageDto;
-import Capstone.Davinchi_Server.chat.repository.UserChatRoomRepository;
 import Capstone.Davinchi_Server.chat.service.TextMessageService;
 import Capstone.Davinchi_Server.global.exception.ApiException;
 import Capstone.Davinchi_Server.global.exception.ApiResponse;
 import Capstone.Davinchi_Server.user.entity.User;
 import Capstone.Davinchi_Server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -21,38 +20,36 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Slf4j
+
 @RequiredArgsConstructor
 @RestController
 public class TextMessageController {
 
     private final SimpMessageSendingOperations template;
-    private final ChatRoomService chatRoomService;
     private final UserRepository userRepository;
     private final TextMessageService textMessageService;
 
-    // localhost:8080/pub/messageDto/enterUser와 같은 URI로 WebSocket 메시지를 전송하면
-    // 해당 요청이 서버에 도달하고, enterUser 메서드가 실행, 이는 클라이언트가 채팅방에 입장할 때 trigger되는 메소드
-    @MessageMapping("/messageDto/enter-user")
-    public ApiResponse<String> enterUser(@Payload TextMessageDto textMessageDto, SimpMessageHeaderAccessor headerAccessor) {
+    // localhost:8080/pub/enter/{roomId} URI로 WebSocket 접근시
+    // 해당 요청이 서버에 도달 enterUser 메서드 실행, 이는 클라이언트가 채팅방에 입장할 때 trigger되는 메소드
+    @MessageMapping("/enter/{roomId}")
+    public ApiResponse<String> enterUser(@DestinationVariable String roomId, @Payload TextMessageReq.ChatEnterReq chatEnterReq, SimpMessageHeaderAccessor headerAccessor) {
         try {
-            textMessageService.enterUser(textMessageDto, headerAccessor);
+            TextMessageRes.ChatEnterRes chatEnterRes = textMessageService.enterUser(roomId, chatEnterReq, headerAccessor);
+            template.convertAndSend("/sub/chat/room/" + roomId, chatEnterRes);
             return new ApiResponse<>("채팅방 입장 처리를 완료하였습니다.");
         } catch (ApiException ignored) {
             return new ApiResponse<>(ignored.getStatus());
         }
     }
 
-    // 메시지를 보낼 때는 /sub/chat/room/{roomId}를 대상으로 메시지 전송
-    // 클라이언트가 localhost:8080/pub/chat/sendMessage와 같은 URI로 WebSocket 메시지를 전송
-    @MessageMapping("/chat/send-message")
-    public ApiResponse<String> sendMessage(@Payload TextMessageDto textMessageDto) {
+    // 메시지 보낼 때는 /sub/chat/room/{roomId}를 대상으로 메시지 전송
+    // 클라이언트가 localhost:8080/pub/talk/{roomId} URI로 WebSocket 메시지 전송
+    @MessageMapping("/talk/{roomId}")
+    public ApiResponse<String> sendMessage(@DestinationVariable String roomId, @Payload TextMessageReq.ChatTalkReq chatTalkReq) {
         try{
-
-            textMessageService.sendMessage(textMessageDto);
-            template.convertAndSend("/sub/chat/room/" + textMessageDto.getRoomId(), textMessageDto);
+            TextMessageRes.ChatTalkRes chatTalkRes = textMessageService.sendMessage(roomId, chatTalkReq);
+            template.convertAndSend("/sub/chat/room/" + roomId, chatTalkRes);
             return new ApiResponse<>("채팅메시지 처리를 완료하였습니다.");
         } catch (ApiException ignored) {
             return new ApiResponse<>(ignored.getStatus());
@@ -71,20 +68,16 @@ public class TextMessageController {
         String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
 
 
-        // 채팅방 유저 리스트에서 User ID 유저 닉네임 조회 및 리스트에서 유저 삭제
-        // 만약 웹 소켓 연결이 끊어졌을 때 아예 채팅방에서 나가게끔 만들고 싶으면 주석 해제
-        // userChatRoomRepository.deleteUserChatRoomByUserIdWithRoomId(userId, roomId);
-
         if (userId != null) {
             User user = userRepository.findByUserId(userId).orElse(null);
-            TextMessageDto textMessageDto = TextMessageDto.builder()
+            TextMessageRes.ChatLeaveRes chatLeaveRes = TextMessageRes.ChatLeaveRes.builder()
                     .type(TextMessage.TextMessageType.LEAVE)
                     .senderId(userId)
                     .message(user.getNickname() + " 님의 연결이 끊어졌습니다.")
                     .sendTime(LocalDateTime.now().toString())
                     .build();
 
-            template.convertAndSend("/sub/chat/room/" + roomId, textMessageDto);
+            template.convertAndSend("/sub/chat/room/" + roomId, chatLeaveRes);
         }
     }
 
--- src/main/java/Capstone/Davinchi_Server/chat/service/TextMessageService.java
+++ src/main/java/Capstone/Davinchi_Server/chat/service/TextMessageService.java
@@ -1,6 +1,7 @@
 package Capstone.Davinchi_Server.chat.service;
 
-import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageDto;
+import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageReq;
+import Capstone.Davinchi_Server.chat.dto.text_message.TextMessageRes;
 import Capstone.Davinchi_Server.chat.entity.ChatRoom;
 import Capstone.Davinchi_Server.chat.entity.TextMessage;
 import Capstone.Davinchi_Server.chat.repository.ChatRoomRepository;
@@ -16,7 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TextMessageService {
     private final TextMessageRepository textMessageRepository;
@@ -25,36 +25,51 @@ public class TextMessageService {
     private final SimpMessageSendingOperations template;
     private final ChatRoomRepository chatRoomRepository;
 
-    public void enterUser(TextMessageDto textMessageDto, SimpMessageHeaderAccessor headerAccessor) {
-        User user = userRepository.findByUserId(textMessageDto.getSenderId()).orElseThrow(() -> {
+    @Transactional
+    public TextMessageRes.ChatEnterRes enterUser(String roomId, TextMessageReq.ChatEnterReq chatEnterReq, SimpMessageHeaderAccessor headerAccessor) {
+        User user = userRepository.findByUserId(chatEnterReq.getSenderId()).orElseThrow(() -> {
             throw new ApiException(ApiResponseStatus.NONE_EXIST_USER);
         });
-        Long userId = textMessageDto.getSenderId();
+        Long userId = chatEnterReq.getSenderId();
         // 반환 결과를 socket session 에 user ID 로 저장
         headerAccessor.getSessionAttributes().put("userId", userId); // getSessionAttributes()는 WebSocket 세션의 속성(attribute) 맵에 접근하는 메서드
-        headerAccessor.getSessionAttributes().put("roomId", textMessageDto.getRoomId());
-        textMessageDto.setMessage(user.getNickname() + " 님이 입장하셨습니다.");
-        template.convertAndSend("/sub/messageDto/room/" + textMessageDto.getRoomId(), textMessageDto);
+        headerAccessor.getSessionAttributes().put("roomId", roomId);
+        TextMessageRes.ChatEnterRes chatEnterRes= TextMessageRes.ChatEnterRes.builder()
+                .type(TextMessage.TextMessageType.ENTER)
+                .message((user.getNickname() + " 님이 입장하셨습니다."))
+                .sendDate(chatEnterReq.getSendDate())
+                .sendTime(chatEnterReq.getSendTime()).build();
+        return chatEnterRes;
     }
 
-    public void sendMessage(TextMessageDto textMessageDto) {
-        User sender = userRepository.findByUserId(textMessageDto.getSenderId()).orElseThrow(() -> {
+
+
+    @Transactional
+    public TextMessageRes.ChatTalkRes sendMessage(String roomId, TextMessageReq.ChatTalkReq chatTalkReq) {
+        User sender = userRepository.findByUserId(chatTalkReq.getSenderId()).orElseThrow(() -> {
             throw new ApiException(ApiResponseStatus.NONE_EXIST_USER);
         });
-        ChatRoom chatRoom = chatRoomRepository.findChatRoomById(textMessageDto.getRoomId()).orElseThrow(() -> {
+        ChatRoom chatRoom = chatRoomRepository.findChatRoomById(roomId).orElseThrow(() -> {
             throw new ApiException(ApiResponseStatus.NONE_EXIST_REVIEW);
         });
 
         TextMessage chatMessage = TextMessage.builder()
                 .sender(sender)
                 .chatRoom(chatRoom)
-                .textMessageType(textMessageDto.getType())
-                .message(textMessageDto.getMessage())
-                .sendDate(textMessageDto.getSendDate())
-                .sendTime(textMessageDto.getSendTime())
+                .textMessageType(TextMessage.TextMessageType.TALK)
+                .message(chatTalkReq.getMessage())
+                .sendDate(chatTalkReq.getSendDate())
+                .sendTime(chatTalkReq.getSendTime())
                 .build();
         textMessageRepository.save(chatMessage);
 
-
+        TextMessageRes.ChatTalkRes chatTalkRes= TextMessageRes.ChatTalkRes.builder()
+                .senderId(sender.getUserId())
+                .senderNickName(sender.getNickname())
+                .type(TextMessage.TextMessageType.TALK)
+                .message(chatTalkReq.getMessage())
+                .sendDate(chatMessage.getSendDate())
+                .sendTime(chatMessage.getSendTime()).build();
+        return chatTalkRes;
     }
 }
