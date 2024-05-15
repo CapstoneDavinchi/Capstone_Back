8484fa31342432c3cd87d54e7a60d517d27ddf21
이다빈 <dlekqls6204@naver.com>
2024-05-16 04:55:32 +0900

    :zap: fix: webSocketConfig 수정 요청 url 수정, 어노테이션 추가 및 SockJS 제거 Resolves: #16

--- src/main/java/Capstone/Davinchi_Server/global/config/webSocketConfig.java
+++ src/main/java/Capstone/Davinchi_Server/global/config/webSocketConfig.java
@@ -1,20 +1,29 @@
 package Capstone.Davinchi_Server.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
+@EnableWebSocketMessageBroker
+@Configuration
+@RequiredArgsConstructor
 public class webSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+             //   .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+        // 메시지를 구독하는 요청 url -> 메시지를 받을 때
+        registry.enableSimpleBroker("/sub");
+        // 메시지를 발행하는 요청 url -> 메시지를 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
+
+
 }
