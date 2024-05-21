7f4daceea11c722fc666cfee3d019f0172f5d849
zzansol <chsk9912@gmail.com>
2024-05-21 19:45:02 +0900

    :sparkles: feat: 갤러리 게시글 작성 컨트롤러 완성
    갤러리 게시글 작성 컨트롤러 완성
    
    Fixes: #31

--- src/main/java/Capstone/Davinchi_Server/gallery/controller/GalleryPostController.java
+++ src/main/java/Capstone/Davinchi_Server/gallery/controller/GalleryPostController.java
@@ -1,6 +1,10 @@
 package Capstone.Davinchi_Server.gallery.controller;
 
 import Capstone.Davinchi_Server.gallery.dto.GalleryPostReq;
+import Capstone.Davinchi_Server.gallery.dto.GalleryPostRes;
+import Capstone.Davinchi_Server.gallery.entity.GalleryPost;
+import Capstone.Davinchi_Server.gallery.service.GalleryPostCommandService;
+import Capstone.Davinchi_Server.global.exception.ApiException;
 import Capstone.Davinchi_Server.global.exception.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +23,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/gallery")
 public class GalleryPostController {
+    private final GalleryPostCommandService galleryPostCommandService;
     @Operation(summary = "예술작품 등록 API")
-    @PostMapping(value = "/notices", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(value = "/posts", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @PreAuthorize("isAuthenticated()")
-    public void addGalleryPost(@RequestPart GalleryPostReq.AddGalleryPostReq addGalleryPostReq,
-                                               @RequestPart(required = false) List<MultipartFile> images,
-                                               Principal principal){
+    public ApiResponse<GalleryPostRes.AddGalleryPostRes> addGalleryPost(@RequestPart GalleryPostReq.AddGalleryPostReq addGalleryPostReq,
+                                                      @RequestPart(required = false) List<MultipartFile> images,
+                                                      Principal principal){
+        System.out.println(principal.getName());
+        try {
+            return new ApiResponse<>(galleryPostCommandService.addGalleryPost(addGalleryPostReq, images, principal.getName()));
 
+        } catch (ApiException exception) {
+            return new ApiResponse<>(exception.getStatus());
+        }
     }
 }
