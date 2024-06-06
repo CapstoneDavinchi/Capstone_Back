fcbb196271964c130ff6c4cb8322f1301386052c
이다빈 <dlekqls6204@naver.com>
2024-06-07 01:56:20 +0900

    :sparkles: feat: 마켓 Api Swagger 추가 Resolved #56

--- src/main/java/Capstone/Davinchi_Server/market/controller/MarketController.java
+++ src/main/java/Capstone/Davinchi_Server/market/controller/MarketController.java
@@ -5,7 +5,10 @@ import Capstone.Davinchi_Server.global.exception.ApiResponse;
 import Capstone.Davinchi_Server.market.dto.MarketReq;
 import Capstone.Davinchi_Server.market.dto.MarketRes;
 import Capstone.Davinchi_Server.market.service.MarketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,12 +17,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "마켓 게시글 API")
 @RequestMapping("/market")
 public class MarketController {
 
     private final MarketService marketService;
 
-    @PostMapping
+    @Operation(summary = "마켓 게시글 등록 API")
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ApiResponse<String> createMarketPost(Principal principal, @RequestPart(value="image", required = false) List<MultipartFile> multipartFiles,
                                                 @RequestPart(value="postMarketReq") MarketReq.PostMarketReq postMarketReq) {
         try {
@@ -30,7 +35,8 @@ public class MarketController {
         }
     }
 
-    @PatchMapping
+    @Operation(summary = "마켓 게시글 수정 API")
+    @PatchMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ApiResponse<String> modifyMarketPost(Principal principal, @RequestPart(value = "image", required = false) List<MultipartFile> multipartFiles,
                                                 @RequestPart(value = "patchMarketReq") MarketReq.PatchMarketReq patchMarketReq) {
         try {
@@ -40,7 +46,7 @@ public class MarketController {
             return new ApiResponse<>(exception.getStatus());
         }
     }
-
+    @Operation(summary = "마켓 게시글 삭제 API")
     @DeleteMapping("/{marketId}")
     public ApiResponse<String> deleteMarketPost(Principal principal, @PathVariable(name = "marketId") Long marketId){
         try{
@@ -49,7 +55,7 @@ public class MarketController {
             return new ApiResponse<>(exception.getStatus());
         }
     }
-
+    @Operation(summary = "마켓 게시글 전체조회 API")
     @GetMapping
     public ApiResponse<List<MarketRes.MarketListRes>> getMarketPosts(Principal principal) {
         try{
@@ -58,7 +64,7 @@ public class MarketController {
             return new ApiResponse<>(exception.getStatus());
         }
     }
-
+    @Operation(summary = "마켓 게시글 상세조회 API")
     @GetMapping("/{marketId}")
     public ApiResponse<MarketRes.MarketDetailRes> getMarketDetailPost(Principal principal, @PathVariable(name = "marketId") Long marketId) {
         try{
@@ -68,6 +74,7 @@ public class MarketController {
         }
     }
 
+    @Operation(summary = "마켓 게시글 좋아요/좋아요 취소 API")
     @PostMapping("/like/{marketId}")
     public ApiResponse<String> likeMarket(Principal principal, @PathVariable(name="marketId") Long marketId){
         try{
@@ -76,7 +83,7 @@ public class MarketController {
             return new ApiResponse<>(exception.getStatus());
         }
     }
-
+    @Operation(summary = "마켓 게시글 키워드 검색 API")
     @GetMapping("/search")
     public ApiResponse<List<MarketRes.MarketListRes>> searchMarket(Principal principal, @RequestParam("keyword") String keyword){
         try{
@@ -86,6 +93,7 @@ public class MarketController {
         }
     }
 
+    @Operation(summary = "내가 찜한 마켓 게시글 전체조회 API")
     @GetMapping("/my")
     public ApiResponse<List<MarketRes.MarketListRes>> myMarket(Principal principal){
         try{
