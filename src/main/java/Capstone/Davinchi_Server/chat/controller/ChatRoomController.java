16cea8e4fc5158d74ccd6c728b3330cc00ce145c
이다빈 <dlekqls6204@naver.com>
2024-06-07 02:01:02 +0900

    :sparkles: feat: 채팅 Api Swagger 추가 Resolved #56

--- src/main/java/Capstone/Davinchi_Server/chat/controller/ChatRoomController.java
+++ src/main/java/Capstone/Davinchi_Server/chat/controller/ChatRoomController.java
@@ -4,6 +4,8 @@ import Capstone.Davinchi_Server.chat.dto.chatroom.ChatRoomRes;
 import Capstone.Davinchi_Server.chat.service.ChatRoomService;
 import Capstone.Davinchi_Server.global.exception.ApiException;
 import Capstone.Davinchi_Server.global.exception.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +14,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "채팅 API")
 @RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     // 채팅방 생성
+    @Operation(summary = "채팅방 생성 API")
     @PostMapping("/room/{authorId}")
     public ApiResponse<String> CreateChatRoom(@PathVariable Long authorId, Principal principal) {
         try {
@@ -28,6 +32,7 @@ public class ChatRoomController {
     }
 
     // 내가 속한 채팅방 리스트(+상대 프로필) 조회
+    @Operation(summary = "내가 속한 채팅방 리스트 조회 API")
     @GetMapping("/room")
     public ApiResponse<List<ChatRoomRes.GetChatRoomRes>> getChatRoomList(Principal principal) {
         try {
@@ -39,6 +44,7 @@ public class ChatRoomController {
     }
 
     // 채팅방 재입장시 화면에 나와야 할 정보(채팅정보)를 반환
+    @Operation(summary = "채팅방 재입장 시 채팅정보 조회 API")
     @GetMapping("/room/enter/{roomId}")
     public ApiResponse<List<ChatRoomRes.GetChatRoomDetailRes>> getChatRoomDetails(@PathVariable String roomId) {
         try {
@@ -50,6 +56,7 @@ public class ChatRoomController {
 
 
     // 채팅방 나가기/삭제, 이 api 호출시 db에서 해당 room 완전 삭제(거래 1:1진행 전제)
+    @Operation(summary = "채팅방 삭제 API")
     @DeleteMapping("/room/{roomId}")
     public ApiResponse<String> exitChatRoom(@PathVariable String roomId, Principal principal){
         try {
